62e971a7f22f7ba544ad139e0a112231cb6c49aa
ramiro <ramiroreinaldo12@gmail.com>
2022-08-12 00:18:17 -0300

    created table pattern and pieces colors

--- chess.cpp
+++ chess.cpp
@@ -12,11 +12,20 @@ string piecesFigures[2][7] = {{"♕", "♔", "♗", "♖" , "♘", "♙", "■"}
 
 struct Piece {
     int type; 
+    int color;
     float value;
     bool checkMove(vector< vector<int> > positions){
         return false;
     };
-    Piece(int t): type(t), value(piecesValues[t]) {};
+
+    string getFigure(int x, int y){
+        if(type == 6){
+            if(x % 2 == 0 && y % 2 != 0 || x % 2 != 0 && y % 2 == 0) return piecesFigures[1][type];
+            else return piecesFigures[0][type];
+        }
+        return piecesFigures[color][type];
+    }
+    Piece(int t, int pos): type(t), value(piecesValues[t]), color(pos >= 4 ? 1 : 0) {};
 };
 
 struct Chess {
@@ -26,61 +35,52 @@ struct Chess {
 };
 
 int main(){
-    vector< vector<Piece> > initTable (8, vector<Piece> (8, 6));
+    vector< vector<Piece> > initTable;
     //0 => Dama
     //1 => Rey
     //2 => Alfil
     //3 => Torre
     //4 => Caballo
     //5 => Peon
-    for(int i = 0; i < initTable.size(); i++){
-        for(int j = 0; j < initTable[0].size(); j++){
-            if(i > 1 && i < 6) continue;
-            if(i == 1 || i == 6){initTable[i][j] = Piece(5); continue;} 
+    for(int i = 0; i < 8; i++){
+        vector< Piece> newRow;
+        for(int j = 0; j < 8; j++){
+            if(i > 1 && i < 6) {newRow.push_back(Piece(6, i)); continue;} 
+            if(i == 1 || i == 6){newRow.push_back(Piece(5, i)); continue;} 
             switch(j){
                 case 0: case 7:
-                initTable[i][j] = Piece(3);
+                newRow.push_back(Piece(3, i));
                 break;     
                 case 1: case 6:
-                initTable[i][j] = Piece(4);
+                newRow.push_back(Piece(4, i));
                 break;
                 case 2: case 5:
-                initTable[i][j] = Piece(2);
+                newRow.push_back(Piece(2, i));
                 break;
                 case 4:
-                if(j == 0) initTable[i][j] = Piece(1);
-                else initTable[i][j] = Piece(0);
-                break;
+                if(j == 0) {newRow.push_back(Piece(1, i)); break;}
+                newRow.push_back(Piece(0, i)); break;
                 case 3:
-                if(j == 0) initTable[i][j] = Piece(0);
-                else initTable[i][j] = Piece(1);
-                break;
+                if(j == 0) {newRow.push_back(Piece(0, i)); break;}
+                newRow.push_back(Piece(1, i)); break;
             }
         }
+        initTable.push_back(newRow);
     }
 
     while(true){
         for(int i = 0; i <= 8; i++){
             cout << i << " ";
         }
-        system("color %");
         cout << endl;
-        for(int i = 0; i < initTable.size(); i++){
+        for(int i = 0; i < 8; i++){
                 cout << i+1 << " ";
-            for(int j = 0; j < initTable[0].size(); j++){
-                if(initTable[i][j].type == 6){
-                    if(i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0) cout << piecesFigures[1][initTable[i][j].type] << " ";
-                    else cout << piecesFigures[0][initTable[i][j].type] << " ";
-                    continue;
-                }
-
-                if(i > 4 ) cout << piecesFigures[1][initTable[i][j].type] << " ";
-                else cout << piecesFigures[0][initTable[i][j].type] << " ";
+            for(int j = 0; j < 8; j++){
+                cout << initTable[i][j].getFigure(i, j) << " ";
             }
             cout << endl;
         }
         Chess game();
         break;
     }
-
 };
